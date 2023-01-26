f95850c8968acf07bb8ff96916a9103f1c3fb054
Maratuster <92256006+Maratuster@users.noreply.github.com>
2023-01-26 16:49:33 +0700

    Add functions and some rows of code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] GetStringArray(string someString)
+{
+    string[] array = someString.Split(" ", StringSplitOptions.None);
+    return array;
+}
+
+string[] GetThreeDigitWords(string[] array)
+{
+    string[] newArray = {};
+    int i=0;
+    foreach (var item in array)
+    {
+        if (item.Length <4 ) 
+        {
+            Array.Resize(ref newArray, i+1);
+            newArray[i] = item;
+            i++;
+        }
+    }
+
+    return newArray;
+}
+
+Console.WriteLine("Введите текст для проверки задания:");
+string testString = Console.ReadLine();
+string[] testText = GetStringArray(testString);
+string[] testText2 = GetThreeDigitWords(testText);
+Console.WriteLine(string.Join(",", testText));
+Console.WriteLine($"[{string.Join(",", testText2)}]");
